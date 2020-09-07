199a496101ad900c13d610c1d7d647d0c94b3ad8
jcaldwell38 <jcaldwell38@gatech.edu>
2020-09-07 16:09:33 -0400

    adding Event CRUD functionality

--- src/main/java/viapos/api/ShiftsApiController.java
+++ src/main/java/viapos/api/ShiftsApiController.java
@@ -129,7 +129,7 @@ public class ShiftsApiController implements ShiftsApi {
         }
     }
 
-    public ResponseEntity<List<Shift>> getUnassignedShifts(@ApiParam(value = "Content Type" ,required=true) @RequestHeader(value="Content-Type", required=true) String contentType,@ApiParam(value = "Format to send back" ,required=true) @RequestHeader(value="Accept", required=true) String accept, @ApiParam(value = "Day of week to retrieve unassigned shifts for",required=false) @PathVariable("dayOfWeek") String dayOfWeek, @ApiParam(value = "Date to retrieve unassigned shifts for",required=false) @PathVariable("date") String date, @ApiParam(value = "ID of ShiftType to return",required=true) @PathVariable("resources") List<String> resources) {
+    public ResponseEntity<List<Shift>> getUnassignedShifts(@ApiParam(value = "Content Type" ,required=true) @RequestHeader(value="Content-Type", required=true) String contentType,@ApiParam(value = "Format to send back" ,required=true) @RequestHeader(value="Accept", required=true) String accept, @ApiParam(value = "Day of week to retrieve unassigned shifts for",required=false) @RequestParam("dayOfWeek") String dayOfWeek, @ApiParam(value = "Date to retrieve unassigned shifts for",required=false) @RequestParam("date") String date, @ApiParam(value = "ID of ShiftType to return",required=true) @RequestParam("resources") List<String> resources) {
         List<Shift> shifts = shiftService.getUnassignedShifts(dayOfWeek, date, resources);
         if (shifts != null) {
             return new ResponseEntity<List<Shift>>(shifts, HttpStatus.OK);
