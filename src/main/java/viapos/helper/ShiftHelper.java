2431ac8302c987ccd71b0be6bf156efaadb822c5
jcaldwell38 <jcaldwell38@gatech.edu>
2020-09-12 15:08:59 -0400

    adding Event CRUD functionality

--- src/main/java/viapos/helper/ShiftHelper.java
+++ src/main/java/viapos/helper/ShiftHelper.java
@@ -1,10 +1,12 @@
 package viapos.helper;
 
+import org.springframework.stereotype.Component;
 import viapos.model.Employee;
 import viapos.model.Shift;
 
 import java.util.List;
 
+@Component
 public class ShiftHelper {
 
     public void assignShifts(List<Shift> unassignedShifts, List<Employee> employees) {
