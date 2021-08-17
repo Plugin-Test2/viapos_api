1da1eb4ec238410fc6fbac6e5d7dac94d14b3795
jlcaldwell123 <>
2021-08-17 08:45:08 +0200

    Fixing CORS for organizations API

--- src/main/java/viapos/api/OrganizationsApiController.java
+++ src/main/java/viapos/api/OrganizationsApiController.java
@@ -9,11 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import viapos.model.Organization;
 import viapos.service.OrganizationService;
@@ -25,6 +21,7 @@ import java.io.IOException;
 import java.util.List;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2021-08-16T10:41:21.709Z")
 
+@CrossOrigin
 @Controller
 public class OrganizationsApiController implements OrganizationsApi {
 
